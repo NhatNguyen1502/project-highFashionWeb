227f9e25eafaedd217a2d046c2c9c3f29dfbad88
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2023-10-12 08:29:15 +0700

    Update login

--- js/category.js
+++ js/category.js
@@ -6,21 +6,58 @@ fetch(productsApi)
         console.log(products);
         var container = document.getElementById('body');
         var htmls = '';
-        products.forEach((element, index) => {
-            if (index % 4 === 0) {
-                htmls += '<div class="row">';
-            }
+        products.forEach((element) => {
             htmls += `
-                <div class="col-sm-3">
-                    <img class="mb-3" src="../image/Frame 20.png" alt="">
-                    <p class="mb-1">${element.name}</p>
-                    <span class="star">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-                    <p>${element.price}</p>
-                </div>
-            `;
-            if ((index + 1) % 4 === 0 || (index + 1) === products.length) {
-                htmls += '</div>';
-            }
+            <div class="col-sm-3">
+                <img class="mb-3" src="${element.img.url}" alt="">
+                <p class="mb-1">${element.name}</p>
+                <span class="star">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                <p>${element.price}</p>
+            </div>
+        `;
         });
         container.innerHTML = htmls;
     });
+
+ function filterByCategory(categoryName){
+    fetch(productsApi)
+    .then(response => response.json()) 
+    .then((products) => {
+        console.log(products);
+        var container = document.getElementById('body');
+        var htmls = '';
+        products.filter((element)=>element.category===categoryName).forEach((element) => {
+            htmls += `
+            <div class="col-sm-3">
+                <img class="mb-3" src="${element.img.url}" alt="">
+                <p class="mb-1">${element.name}</p>
+                <span class="star">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                <p>${element.price}</p>
+            </div>
+        `;
+        });
+        container.innerHTML = htmls;
+    });
+ }
+ function filterByCategory(categoryName) {
+    fetch(productsApi)
+      .then(response => response.json())
+      .then((products) => {
+        console.log(products);
+        var container = document.getElementById('body');
+        var htmls = '';
+        products.filter((element) => element.category === categoryName).forEach((element) => {
+          htmls += `
+            <div class="col-sm-3">
+              <img class="mb-3" src="${element.img.url}" alt="">
+              <p class="mb-1">${element.name}</p>
+              <span class="star">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+              <p>${element.price}</p>
+            </div>
+          `;
+        });
+        container.innerHTML = htmls;
+      });
+  }
+ 
+
