1de90bf6947f59b7d5d690a2a99ba4b9a0e9f026
Sun <116559833+NhatNguyen1502@users.noreply.github.com>
2023-11-03 00:43:00 +0700

    Checkout (#33)
    * Quyen (#24)
    
    * Update
    
    * Update responsive and header, footer
    
    * create new file user information
    
    * function view all
    
    * add new function view all
    
    * Update responsive and header, footer
    
    * Home page function
    
    * add function product detail
    
    * function product detail
    
    * modify admin
    
    * add function add to cart
    
    ---------
    
    Co-authored-by: Sun <116559833+NhatNguyen1502@users.noreply.github.com>
    
    * done checkout
    
    * update admin
    
    ---------
    
    Co-authored-by: Bquyen0508 <143238500+Bquyen0508@users.noreply.github.com>

--- js/checkout.js
+++ js/checkout.js
@@ -17,8 +17,7 @@ fetch(cartApi)
         let orderTable = document.querySelector('#orderTable');
         let htmls = '';
         let total = 0;
-        let qrProductsData = "Name: " + $('#name').val() + "%0APhone: " + $('#phone').val() + "%0AAddress: " + $('#address').val();
-        
+        let qrProductsData = '';
         fetch(productApi)
             .then(res => res.json())
             .then(productsData => {
@@ -33,64 +32,65 @@ fetch(cartApi)
                         </tr>`;
                     qrProductsData += `%0AName: ${product.name}; Size: ${e.size}; Quantity: ${e.quantity}; Price: $${product.price} `
                     total += parseFloat(product.price);
-                    });
-                    orderTable.innerHTML = htmls;
-                    document.querySelector('#total').innerHTML += `$${total}`
+                });
+                orderTable.innerHTML = htmls;
+                document.querySelector('#total').innerHTML += `$${total}`
             });
         //handle checkout
         let checkoutButton = document.querySelector('#checkout');
         checkoutButton.addEventListener('click', () => {
+            let qr = "Name: " + document.getElementById('name').value + "%0APhone: " + $('#phone').val() + "%0AAddress: " + $('#address').val() + qrProductsData;
             let order = {
-                name: document.querySelector('#name').value,
-                phone: document.querySelector('#phone').value,
-                address: document.querySelector('#address').value,
+                name: $('#name').val(),
+                phone: $('#phone').val(),
+                address: $('#address').val(),
                 userId: userId,
                 products: cart.productsCart,
                 total: total
             };
             document.querySelector('#checkoutBody').innerHTML = `<p class="text-bg-success fs-3 col-sm-6 text-center ms-auto me-auto">Payment success!</p>
                 <div class="text-center m-5">
-                <img id='barcode' src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100" alt="" title="HELLO" width="200" height="200" />
+                    <img id='barcode' src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100" alt="QR payment information" title="QR payment information" width="200" height="200"/>
                 </div>`;
             createOrder(order);
-            updateCart({id:userId, productsCart: []});
-            generateBarCode(qrProductsData, total);
+            updateCart({ id: userId, productsCart: [] });
+            generateBarCode(qr, total);
         });
-    });
+    }); 
 
-function createOrder(data){
-    let option ={
+function generateBarCode(qr, total) {
+    var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + qr + `%0ATotal price: $${total}` + '&amp;size=200x200';
+    $('#barcode').attr('src', url);
+}
+
+function createOrder(data) {
+    let option = {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(data) 
+        body: JSON.stringify(data)
     }
-    fetch(orderApi,option)
+    fetch(orderApi, option)
         .then((response) => response.json())
 }
 
-function updateCart(data){
+function updateCart(data) {
     const options = {
         method: "PUT",
         body: JSON.stringify(data),
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
     }
 
     fetch(`${cartApi}/${userId}`, options)
-    .then(res => res.json())
-}
-function generateBarCode(qrProductsDatats, total) 
-{
-    var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + qrProductsDatats + `%0ATotal price: $${total}` + '&amp;size=50x50';
-    $('#barcode').attr('src', url);
+        .then(res => res.json())
 }
 
-document.querySelector('form').addEventListener('submit', function(event) {
+
+
+document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
 });
-
-let a = [1,2,3,4];
